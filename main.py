ceb86e958bffb461da22b4304c363d136316eff9
elo <blackpeoplearemonkeys@int.pl>
2021-09-03 23:23:25 +0000

    formatting hotfix

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 import re
 from telegram.ext import MessageHandler, Filters
 
-session = HTMLSession()
-
-
+session=HTMLSession()
 def getVideoLink(link):
     try:
         link = re.findall(r' - (.*)', link)[0]
         r = session.get(link)
         soup = BeautifulSoup(r.text, 'html.parser')
-        return soup.find('video').get('data-src')
-    except Exception, e:
+        return soup.find("video").get('data-src')
+    except Exception as e:
         return str('e')
 
 
 from telegram.ext import Updater
 
-updater = Updater(token='1869794186:AAG4uMFkSjgSK2qFGnOJo4-UZw-gMxTGNeA'
-                  , use_context=True)
+updater = Updater(
+    token='1869794186:AAG4uMFkSjgSK2qFGnOJo4-UZw-gMxTGNeA', use_context=True
+)
 dispatcher = updater.dispatcher
 
 
 def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text='Getting video...')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Getting video...')
     videoLink = getVideoLink(update.message.text)
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=videoLink)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=videoLink)
 
 
-echo_handler = MessageHandler(Filters.text & ~Filters.command, echo)
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
 updater.start_polling()
