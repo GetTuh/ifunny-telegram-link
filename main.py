c5ee42cce08080fdfe7da3a96a26f47bc91ad3e2
elo <blackpeoplearemonkeys@int.pl>
2021-12-07 20:18:40 +0000

    Cleaned up code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 import re
@@ -6,44 +8,46 @@ from PIL import Image
 from urllib.request import urlopen
 import os
 
-session=HTMLSession()
+session = HTMLSession()
+
+
 def getVideoLink(link):
     try:
         link = re.findall(r' - (.*)', link)[0]
         type = link.split('/')[3]
         r = session.get(link)
         soup = BeautifulSoup(r.text, 'html.parser')
-        # true if its an image
-        if(type=='picture'):
-            Image.open(urlopen(soup.find_all("img")[1].get('src'))).save('temp.jpg')
-            with Image.open("temp.jpg") as im:
-                width, height = im.size
-                im_crop = im.crop((0, 0, width, height-23))
+        if type == 'picture':
+            Image.open(urlopen(soup.find_all('img')[1].get('src'
+                       ))).save('temp.jpg')
+            with Image.open('temp.jpg') as im:
+                (width, height) = im.size
+                im_crop = im.crop((0, 0, width, height - 23))
                 im_crop.save('temp.jpg')
             return 0
         else:
-            return soup.find("video").get('data-src')
-    except Exception as e:
+            return soup.find('video').get('data-src')
+    except Exception, e:
         return str('Couldn\'t find image or video')
 
 
 from telegram.ext import Updater
 
-updater = Updater(
-    token='1869794186:AAG4uMFkSjgSK2qFGnOJo4-UZw-gMxTGNeA', use_context=True
-)
+updater = Updater(token='1869794186:AAG4uMFkSjgSK2qFGnOJo4-UZw-gMxTGNeA'
+                  , use_context=True)
 dispatcher = updater.dispatcher
 
 
 def echo(update, context):
-    #context.bot.send_message(chat_id=update.effective_chat.id, text='Getting meme... skrrr')
     videoLink = getVideoLink(update.message.text)
     try:
         context.bot.send_video(update.effective_chat.id, videoLink)
     except:
-        context.bot.send_photo(update.effective_chat.id, open('temp.jpg', 'rb'))
+        context.bot.send_photo(update.effective_chat.id, open('temp.jpg'
+                               , 'rb'))
         os.remove('temp.jpg')
 
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+
+echo_handler = MessageHandler(Filters.text & ~Filters.command, echo)
 dispatcher.add_handler(echo_handler)
 updater.start_polling()
