7d25b29c3626a59075c265599f755f065f07c43d
GetTuh <jakubniemczyk97@gmail.com>
2022-10-05 17:55:05 +0200

    Added local ip returning method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from telegram.ext import Updater
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 import re
@@ -7,8 +8,17 @@ from telegram.ext import MessageHandler, Filters
 from PIL import Image
 from urllib.request import urlopen
 import os
+import socket
 
 session = HTMLSession()
+
+
+def getIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = (s.getsockname()[0])
+    s.close()
+    return ip
 
 
 def getVideoLink(link):
@@ -19,7 +29,7 @@ def getVideoLink(link):
         soup = BeautifulSoup(r.text, 'html.parser')
         if type == 'picture':
             Image.open(urlopen(soup.find_all('img')[1].get('src'
-                       ))).save('temp.jpg')
+                                                           ))).save('temp.jpg')
             with Image.open('temp.jpg') as im:
                 (width, height) = im.size
                 im_crop = im.crop((0, 0, width, height - 23))
@@ -31,26 +41,28 @@ def getVideoLink(link):
         return str('Couldn\'t find image or video')
 
 
-from telegram.ext import Updater
-if not(os.path.isfile('.token')):
+if not (os.path.isfile('.token')):
     print('.token file doesn\'t exist. Please create the file and include the Telegram token inside.')
     exit()
 else:
     with open('.token') as f:
         token = f.readlines()[0].strip()
-    updater = Updater(token
-                    , use_context=True)
+    updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
 
 
 def echo(update, context):
-    videoLink = getVideoLink(update.message.text)
-    try:
-        context.bot.send_video(update.effective_chat.id, videoLink)
-    except:
-        context.bot.send_photo(update.effective_chat.id, open('temp.jpg'
-                               , 'rb'))
-        os.remove('temp.jpg')
+    if (update.message.text == 'ip'):
+        context.bot.send_message(
+            text=getIP(), chat_id=update.effective_chat.id)
+    else:
+        videoLink = getVideoLink(update.message.text)
+        try:
+            context.bot.send_video(update.effective_chat.id, videoLink)
+        except:
+            context.bot.send_photo(update.effective_chat.id,
+                                   open('temp.jpg', 'rb'))
+            os.remove('temp.jpg')
 
 
 echo_handler = MessageHandler(Filters.text & ~Filters.command, echo)
